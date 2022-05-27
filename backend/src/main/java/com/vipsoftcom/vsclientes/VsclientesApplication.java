7b60f74b6cad6bffa9a9d2d1ba1a83ce5d46b81e
Ricaro <rickmme@gmail.com>
2022-05-27 16:29:12 -0300

    Project created

---
+++ backend/src/main/java/com/vipsoftcom/vsclientes/VsclientesApplication.java
@@ -0,0 +1,13 @@
+package com.vipsoftcom.vsclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsclientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vipsoftcom/vsclientes/VsclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipsoftcom.vsclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
